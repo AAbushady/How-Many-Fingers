5b498ddd9f742710a14d6f0c52896060ec9ad52e
Alexander Abushady <aabushady214@gmail.com>
2019-07-22 15:08:39 -0400

    Cleaning Up
    Cleaned up code and added comments. Version 1 is complete.

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var guessTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
     
@@ -27,12 +26,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func guessTapped(_ sender: Any) {
+        // Create a random number between 0 and 5.
         let fingers = arc4random_uniform(6)
+        // These if statements will let the user know if they are correct, and also will let them know that they are
+        // using a number that is too large.
         if guessTextField.text != "" {
+            // Convert fingers to a string to compare it to the guess.
             if String(fingers) == guessTextField.text {
                 resultLabel.text = "Correct! You Got It!"
+            // Now we change the guess to an int to make sure that the number is in the appropriate range.
             } else if Int(guessTextField.text!)! > 5 {
                 resultLabel.text = "I don't have more than 5 fingers..."
+            // Feedback for an incorrect answer.
             } else {
                 resultLabel.text = "I'm Sorry it was \(fingers). Try Again!"
             }
